99e8d3a2d03ac7870817f72810c1a57c4fbf287a
Venom-61 <subhendukarmakar61@gmail.com>
2022-11-12 18:58:19 +0530

    Jatayu's Balanced Bracket Sequence

---
+++ c.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+// C. Jatayu's Balanced Bracket Sequence 
+
+void solve() {
+    
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int test = 1;
+    cin >> test;
+    while(test--) {
+        solve();
+    }
+
+    return 0;
+}
